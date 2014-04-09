75c2d2e77d501e1f4ace700fa3d6b91e7f990280
Eric Kelly <heroiceric@gmail.com>
2014-04-09 14:02:40 -0400

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'You post was added.'
+    else
+      flash.now[:alert] = 'There was an issue with your post'
+      render :new
+    end
+  end
+
+  protected
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjaxBlog::Application.config.secret_key_base = 'a5a3d44b94c3ad5f35861ec405bd45af84733a4dc998f1888503f0bd032138eb21f7d4967ee799665db35a8a65d902b5ebb1ac1eeae160a7acc327a4017ef6a6'
