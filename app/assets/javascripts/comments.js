272f499680a15cc7fb093eebe0334ba9457f9c3d
Eric Kelly <heroiceric@gmail.com>
2014-04-10 10:11:00 -0400

    Create DOM elements for comment
    We want our newly added comments to match the style of the other
    comments on the page.
    
    The HTML for the other comments looks something like this:
    
    ```html
    <div class="comment">
      <h4>Eric</h4>
      <p>Hello world</p>
    </div>
    ```
    
    We can use jQuery to create DOM elements as shown in the sample above.
    Instead of appending a string to the comment feed, we can append the
    DOM element that we just created onto the page.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,10 +2,22 @@ $(function() {
   $('#new_comment').on('submit', function(event) {
     event.preventDefault();
 
-    var comment = 'Hello World!'
+    // Comment attributes
+    var comment = {
+      author: 'Aunty Entity',
+      body: 'Welcome, to another edition of Thunderdome!'
+    };
+
+    // Create the HTML/DOM element for the new comment
+    var $commentAuthor = $('<h4>').text(comment.author);
+    var $commentBody = $('<p>').text(comment.body);
+    var $comment = $('<div>')
+      .addClass('comment')
+      .append($commentAuthor)
+      .append($commentBody);
 
     // Add the new comment to the comment feed
     var $commentFeed = $('.comment-feed');
-    $commentFeed.append(comment);
+    $commentFeed.append($comment);
   });
 });
