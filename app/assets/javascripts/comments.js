e89ed0a279484a4883e2b69edd01a8f0c314648c
Eric Kelly <heroiceric@gmail.com>
2014-04-10 11:45:10 -0400

    Create the comment asynchronously
    Rather than hard coding the data, we want to use jQuery’s
    [.ajax()](https://api.jquery.com/jQuery.ajax/) function to send a POST
    request to `/posts/:id/comments` that includes all of the data in our
    form. When the request finishes, either the `success` or the `failure`
    callback function will run, depending on the [HTTP status
    code](http://en.wikipedia.org/wiki/List_of_HTTP_status_codes) of the
    response that the server sends back.
    
    Usually our requests to the server are sending back HTML as the body of
    the response. In order to configure the server to send back JSON as the
    response, you need to change your controller a little bit. This has
    already been done for you but you should go check out the
    `CommentsController` to see how it works.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -8,6 +8,35 @@ $(function() {
       body: 'Welcome, to another edition of Thunderdome!'
     };
 
+    // Get the <form> that was submitted
+    var $form = $(event.currentTarget);
+
+    // Send a request in the background to the specified URL
+    $.ajax({
+      // Set the request type to POST
+      type: 'POST',
+
+       // Get the URL from the <form> tag (/posts/:id/comments)
+      url: $form.attr('action'),
+
+      // Specify the type of data that you're expecting back from the server
+      dataType: 'json',
+
+      // Send all the data from the form fields
+      data: $form.serialize(),
+
+      // This function will run when the response returned is successful.
+      // The body of the response will be passed in as the `response`.
+      success: function(response) {
+        alert('hurray');
+      },
+
+      // This function will run when the response returned is an error.
+      error: function() {
+        alert('Something was wrong with your comment. Try again.');
+      }
+    });
+
     // Create the HTML/DOM element for the new comment
     var $commentAuthor = $('<h4>').text(comment.author);
     var $commentBody = $('<p>').text(comment.body);
