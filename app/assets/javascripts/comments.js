4c55cc8694a2f64838319227070b345671d1b737
Janet Chuang <janet.chuang@gmail.com>
2014-04-22 17:57:43 -0400

    create comment asynchronously, using ajax function

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -7,6 +7,21 @@ $(function() {
       body: 'meow!'
     };
 
+    var $form = $(event.currentTarget);
+
+    $.ajax({
+      type: 'POST',
+      url: $form.attr('action'),
+      dataType: 'json',
+      data: $form.serialize(),
+      success: function(response) {
+        alert('yay!');
+      },
+      error: function() {
+        alert('oops, something was wrong with your comment.')
+      }
+    });
+
     var $commentAuthor = $('<h4>').text(comment.author);
     var $commentBody = $('<p>').text(comment.body);
     var $comment = $('<div>')
