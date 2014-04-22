58abc0c404e42fb7fe4d7eb63a4f92334c272869
Janet Chuang <janet.chuang@gmail.com>
2014-04-22 18:17:08 -0400

    add comment to page using the success callback

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -15,21 +15,19 @@ $(function() {
       dataType: 'json',
       data: $form.serialize(),
       success: function(response) {
-        alert('yay!');
+        var $commentAuthor = $('<h4>').text(comment.author);
+        var $commentBody = $('<p>').text(comment.body);
+        var $comment = $('<div>')
+          .addClass('comment')
+          .append($commentAuthor)
+          .append($commentBody);
+
+        var $commentFeed = $('.comment-feed');
+        $commentFeed.append(comment);
       },
       error: function() {
-        alert('oops, something was wrong with your comment.')
+        alert('Something was wrong with your comment. Please try again.')
       }
     });
-
-    var $commentAuthor = $('<h4>').text(comment.author);
-    var $commentBody = $('<p>').text(comment.body);
-    var $comment = $('<div>')
-      .addClass('comment')
-      .append($commentAuthor)
-      .append($commentBody);
-
-    var $commentFeed = $('.comment-feed');
-    $commentFeed.append(comment);
   });
 });
